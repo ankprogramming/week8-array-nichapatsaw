035224f5eb5ffa2c1f2c3bf4965d2a64ee65186d
Unknown <nichapat18102541@gmail.com>
2018-01-23 11:57:26 +0700

    เสร็จแล้ว ข้อที่ 1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 int main () {
 
+
         int arr[10] = {3,2,5,5,6,9,1,3,2,7};
 
         // เขียนโปรแกรมเพื่อแสดงค่าใน arr
+
+        for (int e=0; e<10; e++){
+            cout << arr[e] << endl;
+        }
 
         return 0;
 }
