ba5e7d0452402fd596e9eba97c58aac4e14c6aff
Unknown <nichapat18102541@gmail.com>
2018-01-23 11:58:09 +0700

    เสร็จแล้ว ข้อที่ 3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -4,6 +4,29 @@ using namespace std;
 int main () {
         // สร้างตัวแปร array ชื่อ calendar เพื่อสร้างที่เก็บปฏิทิน
         // calendar[5][7]
+        int calender[5] [7];
+        int day=1;
+
+        for (int q=0; q<4; q++){
+            calender[0] [q]=0;
+        }
+
+        for (int q=4; q<7; q++){
+            calender[0] [q] =day;
+            day++;
+        }
+        for (int q=1; q<5; q++){
+            for (int e=0; e<7; e++){
+                calender[q][e]=day;
+                day++;
+            }
+        }
+        for (int q=0; q<5;q++){
+            for (int e=0; e<7; e++){
+                cout<<calender[q][e]<< " ";
+        }
+        cout << endl;
+        }
 
 
         // เขียนโปรแกรมแสดงปฏิทินเดือน ธันวาคม /2560
@@ -16,6 +39,15 @@ int main () {
 
         // เขียนโปรแกรมเพื่อรับสัปดาห์ของเดือนจากผู้ใช้ เขียนโปรแกรมเพื่อคัด เฉพาะวันของสัปดาห์
         // 1 หมายถึง สัปดาห์ที่ 1 ของเดือนธันวาคม 2560 จะได้เลข {1, 2}
+
+
+
+
+
+
+
+
+
 
 
         return 0;
